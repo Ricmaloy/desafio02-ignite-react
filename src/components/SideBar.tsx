14568da50a9861aa1841af273ca8b6b67d295c06
Ricardo <ricardo10_2010@hotmail.com>
2021-03-15 22:21:25 -0300

    challenge completed :fire:

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+};
+
+interface ISidebar {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onGenreClick: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, onGenreClick}:ISidebar) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onGenreClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
